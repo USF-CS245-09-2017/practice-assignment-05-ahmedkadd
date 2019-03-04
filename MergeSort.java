67fd3e783ef8c9df224df7dfe74e8e3cd0d4c2ad
ahmedkadd <ahmedkaddoura99@gmail.com>
2019-03-03 21:37:59 -0800

    Completed merge sort algorithm

--- MergeSort.java
+++ MergeSort.java
@@ -1,8 +1,50 @@
 
 public class MergeSort implements SortingAlgorithm {
 	
-	public void sort(int [] a) {
+	public void sort(int[] a) {
+		int n = a.length;
+		
+		if (n < 2) {
+			return; 
+		} else {
+			int middle = n / 2;
+			
+			int[] left = new int[middle];
+			int [] right = new int [n - middle];
+			
+			for (int i = 0; i < middle; i++) {
+				left[i] = a[i];
+			}
+			
+			for (int i = 0; i < n - middle; i++) {
+				right[i] = a[middle + i];
+			}
+			
+			sort(left);
+			sort(right);
+			
+			merge(a, left, right, middle, n - middle);
+		}
 		
 	}
-
+	
+	public void merge(int[] a, int[] left, int[] right, int leftLength, int rightLength) {
+		int l = 0, r = 0, i = 0;
+		
+		while (l < leftLength && r < rightLength) {
+			if (left[l] <= right[r]) {
+				a[i++] = left[l++];
+			} else {
+				a[i++] = right[r++];
+			}
+		}
+		
+		while(l < leftLength) {
+			a[i++] = left[l++];
+		}
+		
+		while(r < rightLength) {
+			a[i++] = right[r++];
+		}
+	}
 }
